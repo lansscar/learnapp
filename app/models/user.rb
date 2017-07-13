ec29272acf0fd75e38b67fc9122bd848ad9802be
lansscar <lansscar@gmail.com>
2017-07-13 13:06:57 +0800

    password-reset without testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_create :create_activation_digest
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 80 }
@@ -34,10 +34,21 @@ class User < ApplicationRecord
 	self.activation_digest = User.digest(activation_token)
 	end
 	def activate
-		update_attrinute(:activated, true)
+		update_attribute(:activated, true)
 		update_attribute(:activated_at, Time.zone.now)
 	end
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
+	end
+        def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
 	end
 end
